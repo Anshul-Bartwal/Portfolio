9c4e805aa8ffb55772102f80a0e8fa632aa9ef09
Anshul Bartwal <anshulbartwal720@gmail.com>
2026-01-14 11:11:09 +0530

    Initial commit and hero sectionwith header

---
+++ portfolio/src/sections/Hero.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { words } from '../constants/index.js'
+import Button from '../components/Button.jsx'
+const Hero = () => {
+  return (
+    <section id="hero" className='relative overflow-hidden'>
+        <div className='absolute top-0 left-0 z-10'>
+            <img src='/images/bg.png' alt='bg'/>
+        </div>
+        <div className='hero-layout'>{/* here hero layout has predefined styles*/}
+            {/* left section: hero content  */}
+            <header className=' flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
+                <div className='flex flex-col gap-7'>
+                    <div className='hero-text'>
+                        <h1>Shaping
+                            <span className='slide'>
+                                <span className='wrapper'>
+                                    {words.map((word) => (
+                                        <span key={word.text} className='flex items-center gap-1 md:gap-3 pb-2'>
+                                            <img src={word.imgPath} alt={word.text} className='xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50'/>
+                                            <span className=''>{word.text}</span>
+                                        </span>
+                                        
+                                    ))}
+                                </span>
+                            </span>
+                        </h1>
+                        <h1>into Real Projects</h1>
+                        <h1>that Deliver Results</h1>
+                    </div>
+                    <p className='text-white-50 md:text-xl relative z-10 pointer-events-none'>Hello! I am Anshul Bartwal, a student of UPES,Uttarakhand</p>
+                    <Button 
+                        className="md:w-80 md:h-16 w-60 h-12"
+                        text="See My Work"
+                    />
+                </div>
+            </header>
+            {/* right section: 3d model */}
+        </div>
+
+    </section>
+  )
+}
+
+export default Hero
