6aeb337c04f37364695e97464e1ef0af011b95fd
Anshul Bartwal <anshulbartwal720@gmail.com>
2026-01-19 21:25:41 +0530

    Animated Showcase

--- portfolio/src/sections/Showcase.jsx
+++ portfolio/src/sections/Showcase.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { useGSAP } from '@gsap/react'
 import { work } from '../constants'
+
 const Showcase = () => {
+
+    const sectionRef = useRef(null);
+    const project1Ref = useRef(null);
+    const project2Ref = useRef(null);
+    const project3Ref = useRef(null);
+
+    gsap.registerPlugin(ScrollTrigger)
+
+    useGSAP(() =>{
+        const projects = [project1Ref.current,project2Ref.current,project3Ref.current];
+        projects.forEach((card,index) =>{
+            gsap.fromTo(card,{
+                y:50,
+                opacity:0
+            },
+            {
+                y:50,
+                opacity:1,
+                duration:1,
+                delay:0.3*(index+1),
+                scrollTrigger:{
+                    trigger:card,
+                    start:'top bottom-=100'
+                }
+            }
+        )
+
+        })
+        gsap.fromTo(sectionRef.current,{ opacity:0},{opacity:1, duration: 1.5})
+    },[])
+
+
   return (
-    <div id="work" className='app-showcase'>
+    <section id="work" className='app-showcase' ref={sectionRef}>
         <div className='w-full'>
             <div className='showcaselayout'>
                 {/* left */}
-                <div className='first-project-wrapper'>
+                <div className='first-project-wrapper' ref={project1Ref}>
                     <div className='image-wrapper'>
                         <img src={work[0].imgPath} />
                     </div>
@@ -19,14 +55,14 @@ const Showcase = () => {
 
                 {/* right */}
                 <div className='project-list-wrapper overflow-hidden'>
-                    <div className='project'>
+                    <div className='project' ref={project2Ref}>
                         <div className='image-wrapper bg-[#ffefdb]' >
                             <img src={work[1].imgPath} />
                         </div>
                         <h2>{work[1].heading}</h2>
                     </div>
 
-                    <div className='project'>
+                    <div className='project' ref={project3Ref}>
                         <div className='image-wrapper bg-[#ffe7bd]' >
                             <img src={work[2].imgPath} />
                         </div>
@@ -37,7 +73,7 @@ const Showcase = () => {
             </div>
         </div>
         
-    </div>
+    </section>
   )
 }
 
